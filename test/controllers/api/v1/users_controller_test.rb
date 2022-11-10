4574401c8ae09434feb23658f78bb8b90d18d8b3
Gaudencia <conny@students.uonbi.ac.ke>
2022-11-10 03:59:49 +0300

    destroy user

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -24,8 +24,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       post api_v1_users_url, params: {user: {email: @user.email,password: "1234"}}, as: :json
     end
    assert_response :unprocessable_entity
-
- 
+  end
+  test 'should update user' do
+  patch api_v1_user_url(@user), params: {user: {email: @user.email,password:'1234'}},as: :json
+  assert_response :success
+  end
+  test 'should not update user when invalid params are sent' do
+    patch api_v1_user_url(@user),params: {user:{email:"bad_email",password:'1234'}},as: :json
+    assert_response :unprocessable_entity
+  end
+  test 'should destroy user' do
+    assert_difference('User.count',-1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :no_content
   end
   end
   # test "the truth" do
